4f41e0c93319b1444bc730aaeade182e06e28d40
azzam911 <ceerato911@gmail.com>
2023-01-21 22:09:55 +0300

    I Add some features and add some new style

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ let signin = document.querySelector(".signin");
 let box=document.querySelector(".box");
 signin.onclick = function() {
             box.style.transform="rotateY(-180deg) translateX(100%)";
-            // box.style.transform="";
-            
 }
 
 let siginup = document.querySelector(".signup");
 siginup.onclick = function () {
     box.style.transform="rotateY(0deg) translateX(0%)";
+}
 
+let labUs = document.querySelector(".l-us")
+let username = document.getElementById("uname");
+
+username.onfocus = function() {
+    labUs.style.top="-12px";
+    labUs.style.backgroundColor="#4D5656";
+    
+    username.onblur = function() {
+        if (username.value=='') {
+            labUs.style.top="7px";
+        }
+    }
 }
